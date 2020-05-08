1b2abd2060d39be46d9734c191f330e67e76ed0b
Łukasz Piotrowski <lukasz.piotrowski@whiteaster.com>
2020-05-08 23:50:56 +0200

    delete rx-form lib

--- apps/admin/src/app/shared/shared.module.ts
+++ apps/admin/src/app/shared/shared.module.ts
@@ -3,9 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { UiModule } from '@libs/ui/src/lib/ui.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RxFormModule } from '@libs/rx-form/src/lib/rx-form.module';
 
-const modules = [CommonModule, ReactiveFormsModule, UiModule, RxFormModule];
+const modules = [CommonModule, ReactiveFormsModule, UiModule];
 
 @NgModule({
   declarations: [],
